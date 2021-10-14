048affc28e5f2fa69d2f68d50a626da9b8c8ca4b
Lucas Sepriano <88594847+LucasSepri@users.noreply.github.com>
2021-10-14 13:01:17 -0300

    Add files via upload
    minha primeira impressão cmd

---
+++ atividade cmd/primeira impressão cmd/me.bat
@@ -0,0 +1,18 @@
+echo off
+title Virus
+pause
+echo "Criando pastas"
+mkdir a b c
+cd a
+mkdir aula1
+cd ..
+cd b
+mkdir aula2
+cd ..
+cd c
+mkdir aula3
+cd ..
+pause
+echo "Agora vai.."
+pause
+shutdown -s -t 300
